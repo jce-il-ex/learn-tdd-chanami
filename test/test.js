c8653e29c5d89025f3c8fb6a2d573c19f28c7e5a
chanami <chanami15@gmail.com>
2017-01-10 18:44:53 +0200

    blue

--- test/test.js
+++ test/test.js
@@ -51,6 +51,9 @@ C.getChange = function (totalPayable, cashPaid) {
 
 
 
+//blue change
+
+
 it('getChange(1487,10000) should equal [5000, 2000, 1000, 500, 10, 2, 1 ]', function(){
 			        assert.deepEqual(C.getChange(1487,10000), [5000, 2000, 1000, 500, 10, 2, 1 ]);
 				});
